c6f3ed5af5f844fe4bf7d71412a68cb1e935be6f
AlinaGoaga <35202557+AlinaGoaga@users.noreply.github.com>
2023-06-30 09:52:45 +0200

    Update tables and extract helpers  (#11)
    * Update hr table
    
    * Bring the OCI repo table changes to the others also
    
    * Extract column helpers
    
    * Update style in name and add pending state for ver
    
    * Implement PR feedback
    
    * Replace icons in kubeStatusIndicator with Backstage provided ones

--- plugins/backstage-plugin-flux/src/components/FluxEntityOciRepositoriesCard/FluxOciRepositoriesTable.tsx
+++ plugins/backstage-plugin-flux/src/components/FluxEntityOciRepositoriesCard/FluxOciRepositoriesTable.tsx
@@ -1,82 +1,26 @@
 import React, { useMemo } from 'react';
-import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 import { Table, TableColumn } from '@backstage/core-components';
-import { DateTime } from 'luxon';
-import { NameLabel, verifiedStatus } from '../helpers';
-import { OCIRepository } from '../../hooks';
-import { automationLastUpdated, useStyles } from '../utils';
-import Flex from '../Flex';
-import KubeStatusIndicator from '../KubeStatusIndicator';
-
-const UrlWrapper = styled.div`
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  direction: rtl;
-  max-width: 350px;
-`;
+import {
+  idColumn,
+  nameAndClusterNameColumn,
+  verifiedColumn,
+  urlColumn,
+  tagColumn,
+  statusColumn,
+  updatedColumn,
+} from '../helpers';
+import { useStyles } from '../utils';
+import { OCIRepository } from '../../objects';
 
 export const defaultColumns: TableColumn<OCIRepository>[] = [
-  {
-    title: 'id',
-    field: 'id',
-    hidden: true,
-  },
-  {
-    title: 'Name',
-    field: 'name',
-    searchable: true,
-    render: (repo: OCIRepository): React.ReactNode => (
-      <Flex column>
-        <NameLabel resource={repo} />
-        <span>{repo.clusterName}</span>
-      </Flex>
-    ),
-  },
-  {
-    title: 'Verified',
-    render: (repo: OCIRepository) => {
-      return verifiedStatus({ resource: repo });
-    },
-  },
-  {
-    title: 'URL',
-    render: (repo: OCIRepository) => {
-      return <UrlWrapper title={repo.url}>{repo.url}</UrlWrapper>;
-    },
-    field: 'url',
-    searchable: true,
-  },
-  {
-    title: 'Tag',
-    render: (repo: OCIRepository) => {
-      return <span>{repo.artifact?.revision.split('@')[0]}</span>;
-    },
-    field: 'revision',
-    searchable: true,
-  },
-  {
-    title: 'Status',
-    render: (repo: OCIRepository) => {
-      return (
-        <KubeStatusIndicator
-          short
-          conditions={repo.conditions}
-          suspended={repo.suspended}
-        />
-      );
-    },
-  },
-  {
-    title: 'Updated',
-    field: 'lastUpdatedAt',
-    render: (repo: OCIRepository) => {
-      return DateTime.fromISO(repo.lastUpdatedAt).toRelative({
-        locale: 'en',
-      });
-    },
-  },
+  idColumn(),
+  nameAndClusterNameColumn(),
+  verifiedColumn(),
+  urlColumn(),
+  tagColumn(),
+  statusColumn(),
+  updatedColumn(),
 ];
 
 type Props = {
@@ -93,29 +37,36 @@ export const FluxOCIRepositoriesTable = ({
   const classes = useStyles();
   // TODO: Simplify this to store the ID and OCIRepository
   const data = ociRepositories.map(or => {
+    const {
+      clusterName,
+      namespace,
+      name,
+      conditions,
+      suspended,
+      url,
+      type,
+      artifact,
+    } = or;
     return {
       // make material-table happy and add an id to each row
       // FIXME: maybe we can tell material-table to use a custome key?
-      id: `${or.clusterName}/${or.namespace}/${or.name}`,
-      conditions: or.conditions,
-      suspended: or.suspended,
-      name: or.name,
-      namespace: or.namespace,
-      url: or.url,
-      clusterName: or.clusterName,
-      type: or.type,
-      artifact: or.artifact,
-      revision: or.artifact?.revision,
-      // can this use lastUpdate: or.lastUpdatedAt ?
-      lastUpdatedAt: automationLastUpdated(or),
-    } as OCIRepository & { id: string; lastUpdatedAt: string };
+      id: `${clusterName}/${namespace}/${name}`,
+      conditions,
+      suspended,
+      name,
+      namespace,
+      url,
+      clusterName,
+      type,
+      artifact,
+    } as OCIRepository & { id: string };
   });
 
   return useMemo(() => {
     return (
       <Table
         columns={columns}
-        options={{ paging: true, search: true, pageSize: 5 }}
+        options={{ padding: 'dense', paging: true, search: true, pageSize: 5 }}
         title="OCI Repositories"
         data={data}
         isLoading={isLoading}
--- plugins/backstage-plugin-flux/src/components/helpers.tsx
+++ plugins/backstage-plugin-flux/src/components/helpers.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { DateTime } from 'luxon';
 import { Link } from '@backstage/core-components';
 import { Tooltip } from '@material-ui/core';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-import { GitRepository, OCIRepository, useWeaveFluxDeepLink } from '../hooks';
-import { VerifiableSource, findVerificationCondition } from './utils';
-import { HelmRelease } from '../objects';
+import { useWeaveFluxDeepLink } from '../hooks';
+import {
+  VerifiableSource,
+  automationLastUpdated,
+  findVerificationCondition,
+  useStyles,
+} from './utils';
+import { GitRepository, HelmRelease, OCIRepository } from '../objects';
+import Flex from './Flex';
+import KubeStatusIndicator from './KubeStatusIndicator';
+
+const UrlWrapper = styled.div`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  direction: rtl;
+  max-width: 350px;
+`;
 
 /**
  * Calculate a Name label for a resource with the namespace/name and link to
@@ -18,13 +35,14 @@ export const NameLabel = ({
   const { name, namespace } = resource;
   const deepLink = useWeaveFluxDeepLink(resource);
   const label = `${namespace}/${name}`;
+  const classes = useStyles();
 
   if (!deepLink) {
-    return <span style={{ fontWeight: 600 }}>{label}</span>;
+    return <span className={classes.nameLabel}>{label}</span>;
   }
 
   return (
-    <Link style={{ fontWeight: 600 }} to={deepLink}>
+    <Link className={classes.nameLabel} to={deepLink}>
       {label}
     </Link>
   );
@@ -42,13 +60,98 @@ export const verifiedStatus = ({
     color = '#27AE60';
   } else if (condition?.status === 'False') {
     color = '#BC3B1D';
+  } else if (condition?.status !== undefined) {
+    color = '#FEF071';
   }
-
-  // TODO: Alternative icon?
 
   return (
     <Tooltip title={condition?.message || ''}>
       <VerifiedUserIcon style={{ color, height: '16px' }} />
     </Tooltip>
   );
+};
+
+export const nameAndClusterName = ({
+  resource,
+}: {
+  resource: HelmRelease | GitRepository | OCIRepository;
+}): JSX.Element => (
+  <Flex column>
+    <NameLabel resource={resource} />
+    <span>{resource.clusterName}</span>
+  </Flex>
+);
+
+export const idColumn = () => {
+  return {
+    title: 'id',
+    field: 'id',
+    hidden: true,
+  };
+};
+
+export const nameAndClusterNameColumn = () => {
+  return {
+    title: 'Name',
+    render: (
+      resource: HelmRelease | GitRepository | OCIRepository,
+    ): React.ReactNode => nameAndClusterName({ resource }),
+  };
+};
+
+export const verifiedColumn = () => {
+  return {
+    title: 'Verified',
+    render: (resource: GitRepository | OCIRepository) => {
+      return verifiedStatus({ resource });
+    },
+  };
+};
+
+export const urlColumn = () => {
+  return {
+    title: 'URL',
+    render: (resource: GitRepository | OCIRepository) => {
+      return <UrlWrapper title={resource.url}>{resource.url}</UrlWrapper>;
+    },
+    field: 'url',
+    searchable: true,
+  };
+};
+
+export const tagColumn = () => {
+  return {
+    title: 'URL',
+    render: (resource: GitRepository | OCIRepository) => {
+      return <span>{resource.artifact?.revision.split('@')[0]}</span>;
+    },
+    field: 'revision',
+    searchable: true,
+  };
+};
+
+export const statusColumn = () => {
+  return {
+    title: 'Status',
+    render: (resource: GitRepository | OCIRepository | HelmRelease) => {
+      return (
+        <KubeStatusIndicator
+          short
+          conditions={resource.conditions}
+          suspended={resource.suspended}
+        />
+      );
+    },
+  };
+};
+
+export const updatedColumn = () => {
+  return {
+    title: 'Updated',
+    render: (resource: GitRepository | OCIRepository | HelmRelease) => {
+      return DateTime.fromISO(automationLastUpdated(resource)).toRelative({
+        locale: 'en',
+      });
+    },
+  };
 };
