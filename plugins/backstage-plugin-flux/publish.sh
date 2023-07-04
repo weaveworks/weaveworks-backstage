4910f80ee86b1a5417e0b3d8bf38085d8273b1dc
Simon Howe <simon@weave.works>
2023-07-04 08:57:34 +0200

    Try publishing from branch

--- plugins/backstage-plugin-flux/publish.sh
+++ plugins/backstage-plugin-flux/publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
+STRIPPED_NEW_VERSION=$(echo $NEW_VERSION | sed -e 's/^v//')
+
 yarn clean
 yarn tsc
 yarn build
-yarn publish
+yarn publish --new-version $STRIPPED_NEW_VERSION
