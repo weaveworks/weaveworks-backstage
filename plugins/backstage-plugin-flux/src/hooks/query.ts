834efd9874d292592e85f816ed736c0e244faecb
AlinaGoaga <goaga.alina@gmail.com>
2023-07-10 12:43:43 +0200

    Remove interval from hook

--- plugins/backstage-plugin-flux/src/hooks/query.ts
+++ plugins/backstage-plugin-flux/src/hooks/query.ts
@@ -213,11 +213,10 @@ export function useHelmRepositories(entity: Entity): Response<HelmRepository> {
  */
 
 export function useFluxDeployments(entity: Entity): Response<Deployment> {
-  const { kubernetesObjects, loading, error } = useCustomResources(
-    entity,
-    [helmReleaseGVK, kustomizationGVK],
-    60000000,
-  );
+  const { kubernetesObjects, loading, error } = useCustomResources(entity, [
+    helmReleaseGVK,
+    kustomizationGVK,
+  ]);
 
   const { data, kubernetesErrors } = toResponse<Deployment>(item => {
     const { kind } = JSON.parse(item.payload as string);
