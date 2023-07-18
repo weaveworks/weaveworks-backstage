429df251c4c5a50289a7ff956835a94ee248f997
Simon Howe <simon@weave.works>
2023-07-18 10:34:47 +0200

    Instructions feedback from user-testing
    - Check that jq is installed before starting
    - "Optional" is a bit unclear, setting the URL is optional, not starting
      the app!

--- startup.sh
+++ startup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# check that jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq is not installed.' >&2
+  exit 1
+fi
+
 export K8S_API_ENDPOINT=$(kubectl config view -o json --minify --output jsonpath="{.clusters[*].cluster.server}")
 export K8S_ACCESS_TOKEN=$(kubectl get secret backstage-secret -o json | jq -r ".data.token | @base64d")
 export K8S_CA_DATA=$(kubectl config view -o json --flatten --output jsonpath="{.clusters[*].cluster.certificate-authority-data}")
