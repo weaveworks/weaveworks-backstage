82ab086f0bb818bab3a409c0329397cc15ffdf4c
Kevin McDermott <bigkevmcd@gmail.com>
2023-07-21 13:31:06 +0100

    Simplify startup script.
    Use the go templating output instead of jq.

--- startup.sh
+++ startup.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# check that jq is installed
-if ! [ -x "$(command -v jq)" ]; then
-  echo 'Error: jq is not installed.' >&2
-  exit 1
-fi
+set -e
 
 export K8S_API_ENDPOINT=$(kubectl config view -o json --minify --output jsonpath="{.clusters[*].cluster.server}")
-export K8S_ACCESS_TOKEN=$(kubectl get secret backstage-secret -o json | jq -r ".data.token | @base64d")
+export K8S_ACCESS_TOKEN=$(kubectl get secret backstage-secret -o go-template='{{ .data.token | base64decode }}')
+
 export K8S_CA_DATA=$(kubectl config view -o json --flatten --output jsonpath="{.clusters[*].cluster.certificate-authority-data}")
 
 yarn dev
-
-
