760b2ccf22dff931f540ebd2ecc596ce5ffa9d16
AlinaGoaga <goaga.alina@gmail.com>
2023-11-21 12:44:30 +0100

    Use fetchApi coming from backstage

--- plugins/backstage-plugin-flux/src/hooks/useGetFluxRelease.ts
+++ plugins/backstage-plugin-flux/src/hooks/useGetFluxRelease.ts
@@ -1,16 +1,17 @@
 import { FluxRelease } from '../objects';
 import { useQuery } from '@tanstack/react-query';
+import { FetchApi, fetchApiRef, useApi } from '@backstage/core-plugin-api';
 
 export const LATEST_FLUX_RELEASE_PATH =
   'https://api.github.com/repos/fluxcd/flux2/releases/latest';
 
-export async function getFluxLatestRelease() {
+export async function getFluxLatestRelease(fetchApi: FetchApi) {
   const headers = {
     Accept: 'application/vnd.github+json',
     'X-GitHub-Api-Version': '2022-11-28',
   };
 
-  const response = await fetch(LATEST_FLUX_RELEASE_PATH, {
+  const response = await fetchApi.fetch(LATEST_FLUX_RELEASE_PATH, {
     headers,
   });
 
@@ -18,9 +19,10 @@ export async function getFluxLatestRelease() {
 }
 
 export function useGetLatestFluxRelease() {
+  const fetchApi = useApi(fetchApiRef);
   const { isLoading, data, error } = useQuery<FluxRelease, Error>({
     queryKey: ['latest_flux_release'],
-    queryFn: () => getFluxLatestRelease(),
+    queryFn: () => getFluxLatestRelease(fetchApi),
     // Use cached data for 1hour.
     staleTime: 1000 * 60 * 60,
   });
